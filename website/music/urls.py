97426deec0cabf78c477194d776df67771c4cf3e
jedelson <edelson.m.jason@gmail.com>
2017-09-17 12:02:26 -0700

    Finished through part 31

--- website/music/urls.py
+++ website/music/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     #/music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     #/music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail,name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailVeiw.as_view(), name='detail'),
 
-    #/music/<album_id>/favorite/
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite,name='favorite'),
+    #/music/album/add
+    url(r'^album/add/$', views.AlbumCreate.as_view(), name='album-add')
 ]
